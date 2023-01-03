f2647fcef67071d597ed01a90df0b1a9e8be63ce
developertariq <dev.tariqul.islam@gmail.com>
2023-01-03 16:26:14 +0600

    Change webpack configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 
   module: {
@@ -29,10 +30,6 @@ module.exports = {
       },
     ],
   },
-
-  optimization: {
-    runtimeChunk: 'single',
-  },
-
+  
   mode: 'development',
 };
