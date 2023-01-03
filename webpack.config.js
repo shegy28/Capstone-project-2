5e3e0c6c5a077cd2e34e025f972f6c6fb8d6e8fc
developertariq <dev.tariqul.islam@gmail.com>
2023-01-03 15:44:03 +0600

    Add webpack configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,4 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
 };
