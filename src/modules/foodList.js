fb641df63ec89fd2122d5cc236d8b277b237d0f2
developertariq <dev.tariqul.islam@gmail.com>
2023-01-04 11:15:42 +0600

    Remove comments from file

--- src/modules/foodList.js
+++ src/modules/foodList.js
@@ -1,11 +1,8 @@
 import { getFoodList, URL } from './fetchApi.js';
-// Using getFoodItems function //
+
 const getMeals = async () => {
-  // create an empty array
   const mealsArr = [];
-  // await api data
   const foodList = await getFoodList(URL);
-  // loop to get individual data and populate important data to our array
   foodList.forEach((food) => {
     const foodObj = {
       id: food.idMeal,
