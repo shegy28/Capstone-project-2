776f8cf7c0d2a400c1cad4475c96b919cb35b1ed
developertariq <dev.tariqul.islam@gmail.com>
2023-01-04 20:59:04 +0600

    Remove unwanted code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './style.css';
 import loadFoodDetails from './modules/populatePopupWindow.js';
-// import { addComment } from './modules/comments.js'
 import populateMeal from './modules/populate.js';
-// let food_ID = '';
 
 window.addEventListener('load', () => {
   populateMeal();
@@ -12,24 +10,6 @@ const openPopupWindow = async (foodId) => {
   await loadFoodDetails(foodId);
   document.body.style.overflowY = 'hidden';
   document.querySelector('#popup').style.display = 'block';
-  // food_ID = foodId;
 };
 
 window.openPopupWindow = openPopupWindow;
-
-// const form = document.getElementById('form');
-
-// const clearForm = () => {
-//   form.elements.username.value = '';
-//   form.elements.comment.value = '';
-// };
-
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   const comments = {};
-//   comments.item_id = food_ID;
-//   comments.username = form.elements.username.value;
-//   comments.comment = form.elements.comment.value;
-//   addComment(comments);
-//   clearForm();
-// });
