b8ecb65a005e3a1d994245ef75f5557555bce28b
shegy28 <shegy28@gmail.com>
2023-01-05 20:43:11 +0200

    Add test case for when length is empty(0)

--- _test_/countComments.test.js
+++ _test_/countComments.test.js
@@ -17,4 +17,21 @@ describe('Counter tests', () => {
     const count = countComments(list);
     expect(count).toBe(3);
   });
+
+  test('Counts loaded comments in list and returns 0 if list.lenght empty', () => {
+    const arr = [];
+    document.body.innerHTML = `
+    <ul id="list">
+    </ul>
+    `;
+    const list = document.getElementById('list');
+    for (let i = 0; i < arr.length; i += 1) {
+      const item = arr[i];
+      const li = document.createElement('li');
+      li.textContent = item;
+      list.appendChild(li);
+    }
+    const count = countComments(list);
+    expect(count).toBe(0);
+  });
 });
